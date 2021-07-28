f6f189a1b6037f8a31e0e1f0ce304caf5fad76c0
Samantha Yuhan <sysweettooth1@gmail.com>
2021-07-27 23:13:24 -0700

    create index.js to ask question with inquirer (only added view functions)

---
+++ index.js
@@ -0,0 +1,56 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql2");
+var tbl = require("console.table");
+var view = require("./lib/view");
+var add = require("./lib/add");
+var update = require("./lib/update");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Ilove2code!",
+  database: "business_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+      throw err;
+  }
+  exports.question();
+});
+
+exports.question = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "choice",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "Update Role",
+                "Quit"                
+            ]
+        }
+    ])
+    .then(function(answer) {
+      if(answer.choice === "View All Departments") {
+        view.viewDepts();
+      }
+      else if(answer.choice === "View All Roles") {
+        view.viewRoles();
+      }
+      else if(answer.choice === "View All Employees") {
+        view.viewEmployees();
+      }
+      else if(answer.choice === "Quit") {
+        connection.end();
+        return ;
+      }
+    });
+};
